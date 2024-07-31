456905cb1ff406865cd52ff512ceab0b86d4798b
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-07-31 12:52:35 +0300

    Update Front

--- ecommerce-html-template/auth.js
+++ ecommerce-html-template/auth.js
@@ -1,31 +1,3 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     // Check login status and update the navbar accordingly
-//     updateNavbar();
-
-//     // Event listener for logout
-//     const logoutBtn = document.getElementById('logout');
-//     if (logoutBtn) {
-//         logoutBtn.addEventListener('click', () => {
-//             localStorage.removeItem('loggedIn');
-//             window.location.href = 'index.html';
-//         });
-//     }
-// });
-
-// function updateNavbar() {
-//     const isLoggedIn = localStorage.getItem('loggedIn');
-//     const inSection = document.getElementById('in');
-//     const logoutBtn = document.getElementById('logout');
-
-//     if (isLoggedIn) {
-//         if (inSection) inSection.style.display = 'none';
-//         if (logoutBtn) logoutBtn.style.display = 'inline';
-//     } else {
-//         if (inSection) inSection.style.display = 'inline';
-//         if (logoutBtn) logoutBtn.style.display = 'none';
-//     }
-// }
-// auth.js
 
 function checkLoginStatus() {
     const loggedIn = localStorage.getItem('loggedIn');
